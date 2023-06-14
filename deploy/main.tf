e2c866c209ec5fccb6be2c9f5ea6b6572b92e325
pedro <omenaph@gmail.com>
2023-06-14 00:09:27 -0300

    style: format main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,14 +1,14 @@
 terraform {
-    backend "s3" {
-        bucket = "recipe-app-devops-tfstate"
-        key = "recipe-app.tfstate"
-        region = "us-east-1"
-        encrypt = true
-        dynamodb_table = "recipe-app-api-devops-tf-state-lock"
-    }
+  backend "s3" {
+    bucket         = "recipe-app-devops-tfstate"
+    key            = "recipe-app.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "recipe-app-api-devops-tf-state-lock"
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    version = "~>2.54.0"
+  region  = "us-east-1"
+  version = "~>2.54.0"
 }
