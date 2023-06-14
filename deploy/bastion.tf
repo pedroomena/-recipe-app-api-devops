b9d871a7828baafecd262489730f7b2295b5eba3
pedro <omenaph@gmail.com>
2023-06-14 00:09:57 -0300

    feat: add terraform for bastion instance

---
+++ deploy/bastion.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.0.*.0-kernel-6.1-x86_64"]
+  }
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+}
