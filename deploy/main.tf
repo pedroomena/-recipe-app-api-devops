d84cf6ca5bccbb2cbc6e1695ec4ba05841d10653
pedro <omenaph@gmail.com>
2023-06-15 11:05:13 -0300

    feat[terraform]: add variables and tags

--- deploy/main.tf
+++ deploy/main.tf
@@ -12,3 +12,13 @@ provider "aws" {
   region  = "us-east-1"
   version = "~>2.54.0"
 }
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  commom_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
---
+++ deploy/variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  default     = "raad"
+  description = "Acronym of the project's name"
+}
+
+variable "project" {
+  default = "recipe-app-api-devops"
+}
+
+variable "contact" {
+  default = "omenaph@gmail.com"
+}
