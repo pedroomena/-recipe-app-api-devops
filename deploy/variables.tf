0098153c33df0f9b146e2351527468fa1e109c62
pedro <omenaph@gmail.com>
2023-06-23 16:53:06 -0300

    feat[terraform]: add rds resource

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -10,3 +10,11 @@ variable "project" {
 variable "contact" {
   default = "omenaph@gmail.com"
 }
+
+variable "db_username" {
+  description = "Username for the RDS Postgres instance"
+}
+
+variable "db_password" {
+  description = "Password for the RDS Postgres instance"
+}
