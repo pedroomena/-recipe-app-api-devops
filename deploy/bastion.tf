27425875c40b3690e3c7169258188ab39f6f079a
pedro <omenaph@gmail.com>
2023-06-23 21:47:14 -0300

    feat[terraform]: update bastion security group

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -24,7 +24,6 @@ resource "aws_iam_instance_profile" "bastion" {
   role = aws_iam_role.bastion.name
 }
 
-
 resource "aws_instance" "bastion" {
   ami                  = data.aws_ami.amazon_linux.id
   instance_type        = "t2.micro"
@@ -32,9 +31,52 @@ resource "aws_instance" "bastion" {
   iam_instance_profile = aws_iam_instance_profile.bastion.name
   key_name             = var.bastion_key_name
   subnet_id            = aws_subnet.public_a.id
+  vpc_security_group_ids = [
+    aws_security_group.bastion.id
+  ]
 
   tags = merge(
     local.common_tags,
     map("Name", "${local.prefix}-bastion")
   )
+}
+
+resource "aws_security_group" "bastion" {
+  description = "Control bastion inbound and outbound access"
+  name        = "${local.prefix}-bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tpc"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tpc"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol  = "tpc"
+    from_port = 5432
+    to_port   = 5432
+
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
+  tags = local.common_tags
 }
