f3673ada54a0f080e7fcc1c5fe5fd49a10897244
pedro <omenaph@gmail.com>
2023-06-24 19:05:26 -0300

    feat[terraform]: add ecs task definition

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -22,3 +22,17 @@ variable "db_password" {
 variable "bastion_key_name" {
   default = "recipe-app-api-devops-bastion"
 }
+
+variable "ecr_image_api" {
+  description = "ECR Image for API"
+  default = "366507419979.dkr.ecr.us-east-1.amazonaws.com/recipe-app-api-devops:latest"
+}
+
+variable "ecr_image_proxy" {
+  description = "ECR Image for Proxy"
+  default = "366507419979.dkr.ecr.us-east-1.amazonaws.com/recipe-app-api-proxy:latest"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django app"
+}
