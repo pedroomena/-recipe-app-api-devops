47e60e5b9cbd93c9fc7d07d78176dba2256ea425
pedro <omenaph@gmail.com>
2023-06-23 22:49:38 -0300

    fix[terraform]: typo

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -47,14 +47,14 @@ resource "aws_security_group" "bastion" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    protocol    = "tpc"
+    protocol    = "tcp"
     from_port   = 22
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    protocol    = "tpc"
+    protocol    = "tcp"
     from_port   = 443
     to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
@@ -68,7 +68,7 @@ resource "aws_security_group" "bastion" {
   }
 
   egress {
-    protocol  = "tpc"
+    protocol  = "tcp"
     from_port = 5432
     to_port   = 5432
 
